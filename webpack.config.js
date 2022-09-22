d4cd53e73f4da16e298b05d2b05712a8d26faa3f
Fred-George <f.george@visit.brussels>
2022-09-22 10:38:58 +0200

    Layout updates

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,21 @@ Encore
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
+    .copyFiles({
+        from: './assets/images',
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+        // only copy files matching this pattern
+        pattern: /\.(png|jpg|jpeg|svg)$/
+        })
+
+    .copyFiles({
+        from: './assets/fonts',
+        // optional target path, relative to the output dir
+        to: 'fonts/[path][name].[ext]',
+        // only copy files matching this pattern
+        pattern: /\.(woff)$/
+        })
     /*
      * ENTRY CONFIG
      *
